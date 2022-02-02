cbb4cb8ce633d5bb1d35c21073da39fd2e9521c9
unknown <anterebol@gmail.com>
2022-02-03 01:24:32 +0300

    feat: add utils and constant

---
+++ src/common/constants.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+export const BODY = <HTMLBodyElement>document.querySelector('body');
+export const y = 3;
--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,20 +1,9 @@
-/* eslint-disable prettier/prettier */
-class Render {
-  nameDiv: string;
+/* eslint-disable no-restricted-globals */
+const createDivElement = (className: string): HTMLDivElement => {
+  const element = document.createElement('div');
+  element.className = className;
 
-  innerContent: string;
-
-  constructor(nameDiv: string, innerContent: string) {
-    this.nameDiv = nameDiv;
-    this.innerContent = innerContent;
-  }
-
-  render() {
-    const box = document.createElement('div');
-    box.className = this.nameDiv;
-    box.innerHTML = this.innerContent;
-    return box;
-  }
-}
-// eslint-disable-next-line import/prefer-default-export
-export { Render };
+  return element;
+};
+const checkHash = (): string => location.hash;
+export { createDivElement, checkHash };
