de25e6830cac41bb42103e668eae85e881dc19d7
unknown <anterebol@gmail.com>
2022-02-03 01:20:27 +0300

    feat: add new class

---
+++ src/app/app.ts
@@ -0,0 +1,57 @@
+/* eslint-disable prettier/prettier */
+import Header from '../components/header/header';
+import Aside from '../components/aside-bar/aside-bar';
+import Footer from '../components/footer/footer';
+import { createDivElement, checkHash } from '../common/utils';
+import { BODY } from '../common/constants';
+import routes from '../routing/routing';
+import Error404 from '../pages/errorPage/error404';
+
+class App {
+  private header: Header;
+
+  private aside: Aside;
+
+  private footer: Footer;
+
+  constructor() {
+    this.header = new Header();
+    this.aside = new Aside();
+    this.footer = new Footer();
+  }
+
+  private renderWrapper = (): HTMLDivElement => {
+    const wrapperElement = createDivElement('wrapper');
+    const aside = this.aside.render();
+
+    wrapperElement.append(aside, createDivElement('content-container container-fluid'));
+
+    return wrapperElement;
+  };
+
+  private addComponentsListeners = (): void => {
+    // this.header.addListeners();
+    // this.sidebar.addListeners();
+    // this.footer.addListeners();
+  };
+
+  start = (): void => {
+    BODY.append(this.header.render(), this.renderWrapper(), this.footer.render());
+
+    this.addComponentsListeners();
+  };
+
+  route = (): void => {
+    const mainContainer = document.querySelector('.content-container') as HTMLElement;
+    const parsedURL = checkHash();
+    console.log(parsedURL);
+    const page = routes[parsedURL] || new Error404();
+    mainContainer.innerHTML = '';
+    mainContainer.append(page.render());
+    // clear mainContainerContent
+    // render mainContainerContent -> page.render
+    // page.addListeners
+  };
+}
+
+export default App;
