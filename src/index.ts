37574e2b4b979ec8698c24658f4ae05061a00648
unknown <anterebol@gmail.com>
2022-02-03 01:22:55 +0300

    feat: add start app

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+/* eslint-disable prettier/prettier */
 import './styles/styles.scss';
 import 'bootstrap';
+import App from './app/app';
+
+const app = new App();
+window.addEventListener('load', () => {
+  app.start();
+  app.route();
+});
+window.addEventListener('hashchange', () => app.route());
+// load app.start(), app.route();
+// hashchange app.route();
