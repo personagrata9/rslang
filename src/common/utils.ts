b14aa867ccc484f10174e5111f446c50feb5931f
personagrata9 <ira.bakhur@gmail.com>
2022-02-03 13:48:14 +0300

    refactor: add helpers for creating , divElement, ulElement and LiElement

---
+++ src/common/utils.ts
@@ -0,0 +1,20 @@
+export const createDivElement = (className: string): HTMLDivElement => {
+  const element = document.createElement('div');
+  element.className = className;
+
+  return element;
+};
+
+export const createUlElement = (className: string): HTMLUListElement => {
+  const element = document.createElement('ul');
+  element.className = className;
+
+  return element;
+};
+
+export const createLiElement = (className: string): HTMLLIElement => {
+  const element = document.createElement('li');
+  element.className = className;
+
+  return element;
+};
