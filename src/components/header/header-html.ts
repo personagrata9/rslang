a50a751fce6faff55684d2100395bfd185245aaf
unknown <anterebol@gmail.com>
2022-02-06 02:36:46 +0300

    refactor: add const inner html for footer and header

---
+++ src/components/header/header-html.ts
@@ -0,0 +1,17 @@
+const headerHtml = `
+<div class="header-nav">
+  <div class="left-part-header">
+    <div class="hamburger">
+      <span class="hamburger-line"></span>
+      <span class="hamburger-line"></span>
+      <span class="hamburger-line"></span>
+    </div>
+    <p class="app-logo">
+      <span class="rs">RS</span>
+      <span class="lang">lang</span>
+    </p>  
+  </div>
+  <p class="page-name">Page name</p>
+  <button class="btn btn-outline-light btn-sg px-3" type="submit">Sign in</button>
+</div>`;
+export default headerHtml;
