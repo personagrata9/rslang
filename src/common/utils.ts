34d42a1e51907a58af665a1a71ba0f5478e080d2
personagrata9 <ira.bakhur@gmail.com>
2022-02-05 22:23:55 +0300

    refactor: update helpers; add helpers for creating nav, button, form and input elements

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,42 +1,90 @@
-export const createDivElement = (className: string): HTMLDivElement => {
-  const element = document.createElement('div');
-  element.className = className;
-
-  return element;
-};
+import { Methods } from './types';
 
 export const checkHash = (): string => window.location.hash;
 
-export const createUlElement = (className: string): HTMLUListElement => {
-  const element = document.createElement('ul');
-  element.className = className;
+export const createDivElement = (...classNames: string[]): HTMLDivElement => {
+  const element = document.createElement('div');
+  element.classList.add(...classNames);
 
   return element;
 };
 
-export const createLiElement = (className: string): HTMLLIElement => {
+export const createNavElement = (...classNames: string[]): HTMLElement => {
+  const element = document.createElement('nav');
+  element.classList.add(...classNames);
+
+  return element;
+};
+
+export const createUlElement = (...classNames: string[]): HTMLUListElement => {
+  const element = document.createElement('ul');
+  element.classList.add(...classNames);
+
+  return element;
+};
+
+export const createLiElement = (...classNames: string[]): HTMLLIElement => {
   const element = document.createElement('li');
-  element.className = className;
+  element.classList.add(...classNames);
 
   return element;
 };
 
 export const createHeadingElement = (
   tagName: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6',
-  className: string,
-  textContent: string
+  textContent: string,
+  ...classNames: string[]
 ): HTMLHeadingElement => {
   const element = document.createElement(tagName);
-  element.className = className;
   element.textContent = textContent;
+  element.classList.add(...classNames);
 
   return element;
 };
 
-export const createParagraphElement = (className: string, innerHTML: string): HTMLParagraphElement => {
+export const createParagraphElement = (innerHTML: string, ...classNames: string[]): HTMLParagraphElement => {
   const element = document.createElement('p');
-  element.className = className;
   element.innerHTML = innerHTML;
+  element.classList.add(...classNames);
+
+  return element;
+};
+
+export const createButtonElement = (
+  type: 'button' | 'reset' | 'submit',
+  textContent: string,
+  ...classNames: string[]
+): HTMLButtonElement => {
+  const element = document.createElement('button');
+  element.type = type;
+  element.textContent = textContent;
+  element.classList.add(...classNames);
+
+  return element;
+};
+
+export const createFormElement = (name: string, ...classNames: string[]): HTMLFormElement => {
+  const element = document.createElement('form');
+  element.name = name;
+  element.classList.add(...classNames);
+  element.method = Methods.Post;
+  element.autocomplete = 'on';
+
+  return element;
+};
+
+export const createInputElement = (
+  type: 'text' | 'email' | 'password',
+  id: string,
+  placeholder: string,
+  ...classNames: string[]
+): HTMLInputElement => {
+  const element = document.createElement('input');
+  element.type = type;
+  element.id = id;
+  element.placeholder = placeholder;
+  element.classList.add(...classNames);
+  element.required = true;
 
   return element;
 };
