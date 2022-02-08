6f5d87d4437dbef8421eb7eac6a07c8d32d75a53
Alexei <lexas427@gmail.com>
2022-02-09 01:24:43 +0300

    fix: rebase problems

--- src/pages/sprint/sprint.ts
+++ src/pages/sprint/sprint.ts
@@ -1,8 +1,21 @@
-class Sprint {
-  render(): HTMLDivElement {
-    const minigames = document.createElement('div');
-    minigames.innerHTML = 'Sprint';
-    return minigames;
+import { createElement } from '../../common/utils';
+import ApiPage from '../api-page';
+
+class Sprint extends ApiPage {
+  constructor() {
+    super('sprint');
+  }
+
+  async render(): Promise<void> {
+    const sprintGamePage = createElement('div', ['sprint-container']);
+    sprintGamePage.append(this.createGame());
+    this.contentContainer.append(sprintGamePage);
+  }
+
+  createGame(): HTMLElement {
+    const gamePage = createElement('div', ['sprint-game-container']);
+    return gamePage;
   }
 }
+
 export default Sprint;
