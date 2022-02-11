2edb88070aed459173bcd579edf85cc72174fd8d
Alexei <lexas427@gmail.com>
2022-02-11 21:35:47 +0300

    fix: rebase problems

--- src/pages/api-page.ts
+++ src/pages/api-page.ts
@@ -1,5 +1,5 @@
 import Api from '../api/api';
-import { IWord, ApiPageNameType } from '../common/types';
+import { IWord, PageNameType } from '../common/types';
 
 abstract class ApiPage {
   protected contentContainer = <HTMLDivElement>document.querySelector('.content-container');
@@ -8,17 +8,27 @@ abstract class ApiPage {
 
   protected textbookPage: string;
 
-  protected userId: string | null;
-
   protected api: Api;
 
-  constructor(protected readonly name: ApiPageNameType) {
+  private lastPage: string;
+
+  constructor(protected readonly name: PageNameType) {
     this.name = name;
     this.textbookGroup = localStorage.getItem('group') || '0';
     this.textbookPage = localStorage.getItem('page') || '0';
-    this.userId = localStorage.getItem('UserId');
+    this.lastPage = localStorage.getItem('lastPage') || '';
     this.api = new Api();
   }
+
+  protected getTextbookWordsItems = async (): Promise<IWord[]> => {
+    const words: IWord[] = [];
+
+    await this.api
+      .getWords(this.textbookGroup, this.textbookPage)
+      .then((results) => results.forEach((result: IWord) => words.push(result)));
+
+    return words;
+  };
 
   protected getWordsItems = async (group?: string, page?: string): Promise<IWord[]> => {
     const words: IWord[] = [];
