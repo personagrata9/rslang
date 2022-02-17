91c2c4e1648d7c8d02c626d62aab6933859eae5a
unknown <anterebol@gmail.com>
2022-02-17 13:47:27 +0300

    init: start task main

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,9 +1,12 @@
+import { createElement } from '../../common/utils';
+
 class Main {
   render(): void {
-    console.log('Main');
-    const main = document.createElement('div');
-    main.innerHTML = 'main';
-
+    const main = createElement('div', ['main-container']);
+    const title = createElement('h1', ['main-title'], 'rs-lang');
+    const quote = createElement('h2', ['main-quote'], 'Your assistant in learning English');
+    main.append(title);
+    main.append(quote);
     const contentContainer = <HTMLDivElement>document.querySelector('.content-container');
     contentContainer.append(main);
   }
