6c34a74d9e09eed75d159ca62c372f87444099e5
Ira Bakhur <persona_grata_9@MacBook-Pro-Ira.local>
2022-02-21 11:11:00 +0300

    refactor: change header styles, remove unnecessary img

--- src/components/header/header-html.ts
+++ src/components/header/header-html.ts
@@ -1,9 +1,10 @@
-import logo from './logo';
-
 const headerHtml = `
 <div class="header-nav">
-  <div class="app-logo">${logo}</div> 
-  <p class="page-name">Page name</p>
+  <div class="app-logo">
+    <div class="app-logo-img shadow"></div>
+    <span class="app-logo-rs">RS<span>L</span><span>ang</span></span>
+  </div> 
+  <p class="page-name">Main</p>
   <span class="user-name"></span>
   <button class="btn btn-outline-light btn-sg px-3 open-popup" type="button">Sign in</button>
   <button class="btn btn-outline-light btn-sg px-3 logout hide" type="button">Logout</button>
